5cac1432308b43a7c09a45e632ab5e947b915108
Josh Nussbaum <joshnuss@gmail.com>
2019-04-06 21:04:55 -0400

    Adds circuits_uart

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule Live.IMU.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:circuits_uart, "~> 1.3"}
     ]
   end
 end

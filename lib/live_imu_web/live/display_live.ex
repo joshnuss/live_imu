d3fc09637f13cfccb6480a7243c7f28a0bd7b59b
Josh Nussbaum <joshnuss@gmail.com>
2019-04-07 00:22:02 -0400

    Switch to using broadcast data

--- lib/live_imu_web/live/display_live.ex
+++ lib/live_imu_web/live/display_live.ex
@@ -1,31 +1,39 @@
 defmodule Live.IMUWeb.DisplayLive do
   use Phoenix.LiveView
 
+  import :math, only: [ cos: 1, sin: 1 ]
+
   def render(assigns) do
+    {roll, pitch, yaw} = assigns.position
+
     ~L"""
     <div>
-      <h2 phx-click="boom"><%= inspect @position %></h2>
+      <ul>
+        <li>pitch: <%= pitch %></li>
+        <li>roll: <%= roll %></li>
+        <li>yaw: <%= yaw %></li>
+      </ul>
+
+    <div id="line" style="transform: rotateZ(<%= yaw %>deg) rotateX(<%= pitch %>deg) rotateY(<%= roll %>deg)"></div>
     </div>
     """
   end
 
   def mount(_session, socket) do
-    if connected?(socket) do
-      :timer.send_interval(20, self(), :tick)
-    end
+    Phoenix.PubSub.subscribe(Live.IMU.PubSub, "imu:position")
 
     {:ok, put_position(socket)}
   end
 
-  def handle_info(:tick, socket) do
-    {:noreply, put_position(socket)}
+  def handle_info(position={_, _, _}, socket) do
+    {:noreply, put_position(socket, position)}
   end
 
   def handle_event("nav", _path, socket) do
     {:noreply, socket}
   end
 
-  defp put_position(socket) do
-    assign(socket, position: Live.IMU.Position.get())
+  defp put_position(socket, position \\ {0,0,0}) do
+    assign(socket, position: position)
   end
 end
